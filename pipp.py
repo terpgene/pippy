ec4d8aa445b554590bb4826b54fcc20c939bb14a
Gene Essel <iosdevgene@gmail.com>
2018-06-16 10:36:59 -0400

    Initial commit

---
+++ pipp.py
@@ -0,0 +1,13 @@
+home = "Thailand"
+if home == "Japan":
+    print("Hello, Japan")
+elif home == "Thailand":
+    print("Hello, Thainland!")
+elif home == "India":
+    print("Hello, India!")
+elif home == "China":
+    print("Hello, China")
+else:
+    print("Hello, World!")
+
+    
