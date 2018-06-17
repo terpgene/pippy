2bd1b85b75c12b445322d3eb8320fb5cd2369247
Gene Essel <iosdevgene@gmail.com>
2018-06-16 22:02:32 -0400

    Added some tuples, list and iterables logic

--- containers.py
+++ containers.py
@@ -1,2 +1,25 @@
 hello = "Hello".upper()
 print(hello)
+
+hello2 = "Hello".replace("o", "@")
+print(hello2)
+
+
+
+fruit = ["Apple","Orange","Pear"]
+fruit.append("Banana")
+fruit.append("Pear")
+
+print(fruit)
+
+random = []
+random.append(True)
+random.append(100)
+random.append(1.1)
+random.append("Hello")
+print(random)
+
+print(fruit[0])
+print(fruit[1])
+print(fruit[2])
+print(fruit[3])
