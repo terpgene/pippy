580879548944d0772615aefdf2dd91bb814d34de
Gene Essel <iosdevgene@gmail.com>
2018-06-16 22:53:18 -0400

    Try input function

--- pipp.py
+++ pipp.py
@@ -1,17 +1,38 @@
-home = "Thailand"
-if home == "Japan":
-    print("Hello, Japan")
-elif home == "Thailand":
-    print("Hello, Thainland!")
-elif home == "India":
-    print("Hello, India!")
-elif home == "China":
-    print("Hello, China")
+# home = "Thailand"
+# if home == "Japan":
+#     print("Hello, Japan")
+# elif home == "Thailand":
+#     print("Hello, Thainland!")
+# elif home == "India":
+#     print("Hello, India!")
+# elif home == "China":
+#     print("Hello, China")
+# else:
+#     print("Hello, World!")
+#
+# colors = ["blue", "green", "yellow"]
+# print(colors)
+#
+# colors[2] = "red"
+# print(colors)
+#
+# colors1 = ["orange", "pink", "black"]
+#
+# print(colors + colors1)
+#
+# green = "green"  not in colors
+# print(green)
+
+
+
+
+
+colors2 = ["purple",
+           "orange",
+           "green"]
+
+guess =  input("Guess a color: ")
+if guess in colors2:
+    print("You guessed correctly!")
 else:
-    print("Hello, World!")
-
-colors = ["blue", "green", "yellow"]
-print(colors)
-
-colors[2] = "red"
-print(colors)
+    print("Wrong! Try again.")
