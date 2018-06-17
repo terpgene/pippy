240de0bdb779dddf8c5321fe28f61e9401cb61c2
Gene Essel <iosdevgene@gmail.com>
2018-06-17 07:44:08 -0400

    test code

--- containers.py
+++ containers.py
@@ -1,29 +1,32 @@
-hello = "Hello".upper()
-print(hello)
+# hello = "Hello".upper()
+# print(hello)
 
-hello2 = "Hello".replace("o", "@")
-print(hello2)
-
-
-
-fruit = ["Apple","Orange","Pear"]
-fruit.append("Banana")
-fruit.append("Pear")
-
-print(fruit)
-
-random = []
-random.append(True)
-random.append(100)
-random.append(1.1)
-random.append("Hello")
-print(random)
-
-print(fruit[0])
-print(fruit[1])
-print(fruit[2])
-print(fruit[3])
+# hello2 = "Hello".replace("o", "@")
+# print(hello2)
 
 
-colors = ["blue", "green", "yellow"]
-print(colors)
+
+# fruit = ["Apple","Orange","Pear"]
+# fruit.append("Banana")
+# fruit.append("Pear")
+
+# print(fruit)
+
+# random = []
+# random.append(True)
+# random.append(100)
+# random.append(1.1)
+# random.append("Hello")
+# print(random)
+
+# print(fruit[0])
+# print(fruit[1])
+# print(fruit[2])
+# print(fruit[3])
+
+
+# colors = ["blue", "green", "yellow"]
+# print(colors)
+
+facts = dict()
+
